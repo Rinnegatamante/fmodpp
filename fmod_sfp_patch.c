7e0655e6ed0fe283e8a17e3c556174a7e86be0ab
Rinnegatamante <rinnegatamante@gmail.com>
2024-02-06 11:52:07 +0100

    Fix for wrong sfp patch for FMOD symbols.

--- fmod_sfp_patch.c
+++ fmod_sfp_patch.c
@@ -1,214 +1,283 @@
-FMOD_RESULT F_API FMOD_System_SetSpeakerPosition_sfp(FMOD_SYSTEM *system, FMOD_SPEAKER speaker, float a1, float a2, FMOD_BOOL active) {
-	int f1, f2;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	return FMOD_System_SetSpeakerPosition(system, speaker, f1, f2, active);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_System_SetSpeakerPosition_sfp(FMOD_SYSTEM *system, FMOD_SPEAKER speaker, float a1, float a2, FMOD_BOOL active) {
+	asm volatile(
+				 // skip r0 : argument `system` is not of type float
+				 // skip r1 : argument `speaker` is not of type float
+				 "vmov s0, r2\n" // move r2 to s0 : argument `a1` is of type float
+				 "vmov s1, r3\n" // move r3 to s1 : argument `a2` is of type float
+				 "ldr r2, [sp]\n" // move sp to r2 : freed up for argument `active`
+				 "b FMOD_System_SetSpeakerPosition\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_System_Set3DSettings_sfp(FMOD_SYSTEM *system, float a1, float a2, float a3) {
-	int f1, f2, f3;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	f3 = *(int *)&a3;
-	return FMOD_System_Set3DSettings(system, f1, f2, f3);
+
+__attribute__((naked)) FMOD_RESULT F_API FMOD_System_Set3DSettings_sfp(FMOD_SYSTEM *system, float a1, float a2, float a3) {
+	asm volatile(
+				 // skip r0 : argument `system` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "vmov s1, r2\n" // move r2 to s1 : argument `a2` is of type float
+				 "vmov s2, r3\n" // move r3 to s2 : argument `a3` is of type float
+				 "b FMOD_System_Set3DSettings\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_System_SetGeometrySettings_sfp(FMOD_SYSTEM *system, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_System_SetGeometrySettings(system, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_System_SetGeometrySettings_sfp(FMOD_SYSTEM *system, float a1) {
+	asm volatile(
+				 // skip r0 : argument `system` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_System_SetGeometrySettings\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Sound_SetDefaults_sfp(FMOD_SOUND *sound, float a1, int priority) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Sound_SetDefaults(sound, f1, priority);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Sound_SetDefaults_sfp(FMOD_SOUND *sound, float a1, int priority) {
+	asm volatile(
+				 // skip r0 : argument `sound` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "ldr r1, [r2]\n" // move r2 to r1 : freed up for argument `priority`
+				 "b FMOD_Sound_SetDefaults\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Sound_Set3DMinMaxDistance_sfp(FMOD_SOUND *sound, float a1, float a2) {
-	int f1, f2;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	return FMOD_Sound_Set3DMinMaxDistance(sound, f1, f2);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Sound_Set3DMinMaxDistance_sfp(FMOD_SOUND *sound, float a1, float a2) {
+	asm volatile(
+				 // skip r0 : argument `sound` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "vmov s1, r2\n" // move r2 to s1 : argument `a2` is of type float
+				 "b FMOD_Sound_Set3DMinMaxDistance\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Sound_Set3DConeSettings_sfp(FMOD_SOUND *sound, float a1, float a2, float a3) {
-	int f1, f2, f3;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	f3 = *(int *)&a3;
-	return FMOD_Sound_Set3DConeSettings(sound, f1, f2, f3);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Sound_Set3DConeSettings_sfp(FMOD_SOUND *sound, float a1, float a2, float a3) {
+	asm volatile(
+				 // skip r0 : argument `sound` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "vmov s1, r2\n" // move r2 to s1 : argument `a2` is of type float
+				 "vmov s2, r3\n" // move r3 to s2 : argument `a3` is of type float
+				 "b FMOD_Sound_Set3DConeSettings\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Sound_SetMusicChannelVolume_sfp(FMOD_SOUND *sound, int channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Sound_SetMusicChannelVolume(sound, channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Sound_SetMusicChannelVolume_sfp(FMOD_SOUND *sound, int channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `sound` is not of type float
+				 // skip r1 : argument `channel` is not of type float
+				 "vmov s0, r2\n" // move r2 to s0 : argument `a1` is of type float
+				 "b FMOD_Sound_SetMusicChannelVolume\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_SetVolume_sfp(FMOD_CHANNEL *channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_SetVolume(channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_SetVolume_sfp(FMOD_CHANNEL *channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_SetVolume\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_SetPitch_sfp(FMOD_CHANNEL *channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_SetPitch(channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_SetPitch_sfp(FMOD_CHANNEL *channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_SetPitch\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_SetReverbProperties_sfp(FMOD_CHANNEL *channel, int instance, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_SetReverbProperties(channel, instance, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_SetReverbProperties_sfp(FMOD_CHANNEL *channel, int instance, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 // skip r1 : argument `instance` is not of type float
+				 "vmov s0, r2\n" // move r2 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_SetReverbProperties\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_SetLowPassGain_sfp(FMOD_CHANNEL *channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_SetLowPassGain(channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_SetLowPassGain_sfp(FMOD_CHANNEL *channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_SetLowPassGain\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_SetPan_sfp(FMOD_CHANNEL *channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_SetPan(channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_SetPan_sfp(FMOD_CHANNEL *channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_SetPan\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_SetMixLevelsOutput_sfp(FMOD_CHANNEL *channel, float a1, float a2, float a3, float a4, float a5, float a6, float a7, float a8) {
-	int f1, f2, f3, f4, f5, f6, f7, f8;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	f3 = *(int *)&a3;
-	f4 = *(int *)&a4;
-	f5 = *(int *)&a5;
-	f6 = *(int *)&a6;
-	f7 = *(int *)&a7;
-	f8 = *(int *)&a8;
-	return FMOD_Channel_SetMixLevelsOutput(channel, f1, f2, f3, f4, f5, f6, f7, f8);
+// AAAAA
+
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_SetMixLevelsOutput_sfp(FMOD_CHANNEL *channel, float a1, float a2, float a3, float a4, float a5, float a6, float a7, float a8) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "vmov s1, r2\n" // move r2 to s1 : argument `a2` is of type float
+				 "vmov s2, r3\n" // move r3 to s2 : argument `a3` is of type float
+
+				 "ldr r1, [sp]\n" // temp. store sp in r1
+				 "ldr r2, [sp, #4]\n" // temp. store sp#4 in r2
+				 "ldr r3, [sp, #8]\n" // temp. store sp#8 in r3
+
+				 "vmov s3, r1\n" // move r1 to s3 : argument `a4` is of type float
+				 "vmov s4, r2\n" // move r2 to s4 : argument `a5` is of type float
+				 "vmov s5, r3\n" // move r3 to s5 : argument `a6` is of type float
+
+				 "ldr r1, [sp, #12]\n" // temp. store sp#12 in r1
+				 "ldr r2, [sp, #16]\n" // temp. store sp#16 in r2
+
+				 "vmov s6, r1\n" // move r1 to s6 : argument `a7` is of type float
+				 "vmov s7, r2\n" // move r2 to s7 : argument `a8` is of type float
+
+				 "b FMOD_Channel_SetMixLevelsOutput\n" // jump to original function
+				);
 }
 
-FMOD_RESULT F_API FMOD_Channel_AddFadePoint_sfp(FMOD_CHANNEL *channel, unsigned long long dspclock, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_AddFadePoint(channel, dspclock, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_AddFadePoint_sfp(FMOD_CHANNEL *channel, unsigned long long dspclock, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 // skip r1 && r2 : argument `dspclock` is not of type float
+				 "vmov s0, r3\n" // move r3 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_AddFadePoint\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_SetFadePointRamp_sfp(FMOD_CHANNEL *channel, unsigned long long dspclock, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_SetFadePointRamp(channel, dspclock, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_SetFadePointRamp_sfp(FMOD_CHANNEL *channel, unsigned long long dspclock, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 // skip r1 && r2 : argument `dspclock` is not of type float
+				 "vmov s0, r3\n" // move r3 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_SetFadePointRamp\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_Set3DMinMaxDistance_sfp(FMOD_CHANNEL *channel, float a1, float a2) {
-	int f1, f2;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	return FMOD_Channel_Set3DMinMaxDistance(channel, f1, f2);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_Set3DMinMaxDistance_sfp(FMOD_CHANNEL *channel, float a1, float a2) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "vmov s1, r2\n" // move r2 to s1 : argument `a2` is of type float
+				 "b FMOD_Channel_Set3DMinMaxDistance\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_Set3DConeSettings_sfp(FMOD_CHANNEL *channel, float a1, float a2, float a3) {
-	int f1, f2, f3;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	f3 = *(int *)&a3;
-	return FMOD_Channel_Set3DConeSettings(channel, f1, f2, f3);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_Set3DConeSettings_sfp(FMOD_CHANNEL *channel, float a1, float a2, float a3) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "vmov s1, r2\n" // move r2 to s1 : argument `a2` is of type float
+				 "vmov s2, r3\n" // move r3 to s2 : argument `a3` is of type float
+				 "b FMOD_Channel_Set3DConeSettings\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_Set3DOcclusion_sfp(FMOD_CHANNEL *channel, float a1, float a2) {
-	int f1, f2;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	return FMOD_Channel_Set3DOcclusion(channel, f1, f2);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_Set3DOcclusion_sfp(FMOD_CHANNEL *channel, float a1, float a2) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "vmov s1, r2\n" // move r2 to s1 : argument `a2` is of type float
+				 "b FMOD_Channel_Set3DOcclusion\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_Set3DSpread_sfp(FMOD_CHANNEL *channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_Set3DSpread(channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_Set3DSpread_sfp(FMOD_CHANNEL *channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_Set3DSpread\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_Set3DLevel_sfp(FMOD_CHANNEL *channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_Set3DLevel(channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_Set3DLevel_sfp(FMOD_CHANNEL *channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_Set3DLevel\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_Set3DDopplerLevel_sfp(FMOD_CHANNEL *channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_Set3DDopplerLevel(channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_Set3DDopplerLevel_sfp(FMOD_CHANNEL *channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_Set3DDopplerLevel\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_Set3DDistanceFilter_sfp(FMOD_CHANNEL *channel, FMOD_BOOL custom, float a1, float a2) {
-	int f1, f2;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	return FMOD_Channel_Set3DDistanceFilter(channel, custom, f1, f2);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_Set3DDistanceFilter_sfp(FMOD_CHANNEL *channel, FMOD_BOOL custom, float a1, float a2) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 // skip r1 : argument `FMOD_BOOL custom` is not of type float
+				 "vmov s0, r2\n" // move r2 to s0 : argument `a1` is of type float
+				 "vmov s1, r3\n" // move r3 to s1 : argument `a2` is of type float
+				 "b FMOD_Channel_Set3DDistanceFilter\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Channel_SetFrequency_sfp(FMOD_CHANNEL *channel, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Channel_SetFrequency(channel, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Channel_SetFrequency_sfp(FMOD_CHANNEL *channel, float a1) {
+	asm volatile(
+				 // skip r0 : argument `channel` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Channel_SetFrequency\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_SoundGroup_SetMuteFadeSpeed_sfp(FMOD_SOUNDGROUP *soundgroup, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_SoundGroup_SetMuteFadeSpeed(soundgroup, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_SoundGroup_SetMuteFadeSpeed_sfp(FMOD_SOUNDGROUP *soundgroup, float a1) {
+	asm volatile(
+				 // skip r0 : argument `soundgroup` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_SoundGroup_SetMuteFadeSpeed\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_SoundGroup_SetVolume_sfp(FMOD_SOUNDGROUP *soundgroup, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_SoundGroup_SetVolume(soundgroup, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_SoundGroup_SetVolume_sfp(FMOD_SOUNDGROUP *soundgroup, float a1) {
+	asm volatile(
+				 // skip r0 : argument `soundgroup` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_SoundGroup_SetVolume\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_DSP_SetWetDryMix_sfp(FMOD_DSP *dsp, float a1, float a2, float a3) {
-	int f1, f2, f3;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	f3 = *(int *)&a3;
-	return FMOD_DSP_SetWetDryMix(dsp, f1, f2, f3);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_DSP_SetWetDryMix_sfp(FMOD_DSP *dsp, float a1, float a2, float a3) {
+	asm volatile(
+				 // skip r0 : argument `dsp` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "vmov s1, r2\n" // move r2 to s1 : argument `a2` is of type float
+				 "vmov s2, r3\n" // move r3 to s2 : argument `a3` is of type float
+				 "b FMOD_DSP_SetWetDryMix\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_DSP_SetParameterFloat_sfp(FMOD_DSP *dsp, int index, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_DSP_SetParameterFloat(dsp, index, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_DSP_SetParameterFloat_sfp(FMOD_DSP *dsp, int index, float a1) {
+	asm volatile(
+				 // skip r0 : argument `dsp` is not of type float
+				 // skip r1 : argument `index` is not of type float
+				 "vmov s0, r2\n" // move r2 to s0 : argument `a1` is of type float
+				 "b FMOD_DSP_SetParameterFloat\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_DSPConnection_SetMix_sfp(FMOD_DSPCONNECTION *dspconnection, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_DSPConnection_SetMix(dspconnection, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_DSPConnection_SetMix_sfp(FMOD_DSPCONNECTION *dspconnection, float a1) {
+	asm volatile(
+				// skip r0 : argument `dspconnection` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_DSPConnection_SetMix\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Geometry_AddPolygon_sfp(FMOD_GEOMETRY *geometry, float a1, float a2, FMOD_BOOL doublesided, int numvertices, const FMOD_VECTOR *vertices, int *polygonindex) {
-	int f1, f2;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	return FMOD_Geometry_AddPolygon(geometry, f1, f2, doublesided, numvertices, vertices, polygonindex);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Reverb3D_Set3DAttributes_sfp(FMOD_REVERB3D *reverb3d, const FMOD_VECTOR *position, float a1, float a2) {
+	asm volatile(
+				 // skip r0 : argument `reverb3d` is not of type float
+				 // skip r1 : argument `*position` is not of type float
+				 "vmov s0, r2\n" // move r2 to s0 : argument `a1` is of type float
+				 "vmov s1, r3\n" // move r3 to s1 : argument `a2` is of type float
+				 "b FMOD_Reverb3D_Set3DAttributes\n" // jump to original function
+				 );
 }
 
-FMOD_RESULT F_API FMOD_Geometry_SetPolygonAttributes_sfp(FMOD_GEOMETRY *geometry, int index, float a1, float a2, FMOD_BOOL doublesided) {
-	int f1, f2;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	return FMOD_Geometry_SetPolygonAttributes(geometry, index, f1, f2, doublesided);
-}
-
-FMOD_RESULT F_API FMOD_Reverb3D_Set3DAttributes_sfp(FMOD_REVERB3D *reverb3d, const FMOD_VECTOR *position, float a1, float a2) {
-	int f1, f2;
-	f1 = *(int *)&a1;
-	f2 = *(int *)&a2;
-	return FMOD_Reverb3D_Set3DAttributes(reverb3d, position, f1, f2);
-}
-
-FMOD_RESULT F_API FMOD_Sound_SetMusicSpeed_sfp(FMOD_SOUND *sound, float a1) {
-	int f1;
-	f1 = *(int *)&a1;
-	return FMOD_Sound_SetMusicSpeed(sound, f1);
+__attribute__((naked)) FMOD_RESULT F_API FMOD_Sound_SetMusicSpeed_sfp(FMOD_SOUND *sound, float a1) {
+	asm volatile(
+				 // skip r0 : argument `sound` is not of type float
+				 "vmov s0, r1\n" // move r1 to s0 : argument `a1` is of type float
+				 "b FMOD_Sound_SetMusicSpeed\n" // jump to original function
+				 );
 }
